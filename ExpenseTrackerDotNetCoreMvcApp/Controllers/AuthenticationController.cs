9c13fe7996bac40d8f88855a30c50b4e32a5658e
DejavuNerdPro <dejavunerdprogrammer001@gmail.com>
2023-03-06 15:56:52 +0630

    Authentication Process

---
+++ ExpenseTrackerDotNetCoreMvcApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTrackerDotNetCoreMvcApp.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult AuthenticationIndex()
+        {
+            return View("AuthenticationIndex");
+        }
+    }
+}
--- ExpenseTrackerDotNetCoreMvcApp/Startup.cs
+++ ExpenseTrackerDotNetCoreMvcApp/Startup.cs
@@ -72,7 +72,7 @@ namespace ExpenseTrackerDotNetCoreMvcApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Authentication}/{action=AuthenticationIndex}/{id?}");
             });
         }
     }
